6ec94716d3f60b77b4b5c31cf17453c19ed68e17
ThiryS <thiry.simon1@gmail.com>
2020-07-28 09:34:20 +0200

    corr event

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -5,7 +5,11 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ $event->name }} @if ($event->user->id === Auth::user()->id) <a href="{{ route('events.edit', $event->id) }}">Modifier</a>
+                <div class="card-header">{{ $event->name }}
+                  @auth
+                  @if ($event->user->id === Auth::user()->id)
+                  @endauth
+                  <a href="{{ route('events.edit', $event->id) }}">Modifier</a>
                   <form action="{{ route('events.destroy', $event->id)}}" method="post">
                     @csrf
                     @method('DELETE')
