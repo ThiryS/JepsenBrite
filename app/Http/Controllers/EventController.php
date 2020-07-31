126926c7733f17c506d043882d0841f6f61880a0
ThiryS <thiry.simon1@gmail.com>
2020-07-31 10:48:49 +0200

    corr storage

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -117,9 +117,7 @@ class EventController extends Controller
 
         if(request('image') != null)
          {
-             $imagePath = request('image')->store('uploads', 'public');
-             $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
-             $image->save();
+             $imagePath = Storage::putFile('image', $request->file('image'))->fit(1000, 1000);
          } else {
              $imagePath = $event->image;
          }
