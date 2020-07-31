a8d00585cec13fbca57c88a37e2face144cb9a9f
ThiryS <thiry.simon1@gmail.com>
2020-07-31 09:32:35 +0200

    corr display welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -98,7 +98,7 @@
 
                                     @if ($event == $loop->first)
 
-                                        <div class="card col-md-12" style="margin-bottom: 1em; margin-top: 1em; margin-left: 1rem;">
+                                        <div class="card col-md-12" style="margin-bottom: 1em; margin-top: 1em;">
                                             <img class="card-img-top" src="storage/{{ $event->image }}" alt="Card image cap" style="height: 300px; object-fit: cover;">
                                             <div class="card-body">
                                                 <h5 class="card-title">{{ $event->name }}</h5>
@@ -116,7 +116,7 @@
                                     @endif
 
                                     @if($event != $loop->first)
-                                        <div class="card col-sm-4" style="margin-bottom: 1em; margin-top: 1em; margin-left: 2rem;">
+                                        <div class="card col-sm-4" style="margin-bottom: 1em; margin-right: : 1rem;">
 
                                         <img class="card-img-top" src="storage/{{ $event->image }}" alt="Card image cap">
 
