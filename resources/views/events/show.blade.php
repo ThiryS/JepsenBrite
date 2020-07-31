b88ced2d948da64c1d5f16bcd61054213e0568b8
ThiryS <thiry.simon1@gmail.com>
2020-07-31 16:00:15 +0200

    some corr

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -53,8 +53,12 @@
                       <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Commentaire') }}</label>
 
                       <div class="col-md-6">
-                          <textarea id="comment" data="data-emojiable="true"" class="form-control @error('comment') is-invalid @enderror" name="comment" required>{{ old('comment') }}</textarea>
-
+                          <textarea id="comment" class="form-control @error('comment') is-invalid @enderror" name="comment" required>{{ old('comment') }}</textarea>
+                          <script type="text/javascript">
+  $(document).ready(function() {
+    $("#comment").emojioneArea();
+  });
+</script>
                           @error('comment')
                               <span class="invalid-feedback" role="alert">
                                   <strong>{{ $message }}</strong>
