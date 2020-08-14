5286668c7ae3debb0676a83a6630421af25eabfd
ThiryS <thiry.simon1@gmail.com>
2020-08-14 10:54:49 +0200

    modif things

--- resources/views/admin/adminEvents.blade.php
+++ resources/views/admin/adminEvents.blade.php
@@ -40,7 +40,7 @@
                     <td>{{ date('d-m-Y', strtotime($event->date)) }}</td>
                     <td>{{ $event->address }}</td>
                     <td>
-                        <a href="{{ route('admin.event.edit', $event->id) }}" class="pr-4">Modifier</a>
+                        <a href="{{ route('admin.event.edit', $event->id) }}" class="pr-4">Modifier</a><br>
                         <a href="{{ route('admin.comments.show', $event->id) }}">Modifier les commentaires</a>
                         <form action="{{ route('admin.events.destroy', $event->id)}}" method="post">
                         @csrf
